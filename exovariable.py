67a11aaac2ec3e1008243cb7a2486b7d85ddc8fe
Manu <manu.angot@hotmail.fr>
2023-05-30 14:24:26 +0200

    Addition of Python algo lessons and exercises

--- exovariable.py
+++ exovariable.py
@@ -16,9 +16,43 @@ print("var1 OU var2 = " + str(var1 or var2))
 print("var1 ET var2 = " + str(var1 and var2))
 print("NON var1 = " + str(not var1))
 
-# concaténation
+# Chaîne de caractères
+phrase1 = "Bonjour, j'aime beaucoup le Python !"
+print(phrase1)
+
+# Concaténation
 mot1 = "Bonjour "
 mot2 = "Python !"
 
-phrase = mot1 + mot2 
-print(phrase)
+phrase2 = mot1 + mot2 
+print(phrase2)
+
+# décomposition en lettre
+print (phrase1[0])
+print (phrase1[1])
+print (phrase1[2])
+print (phrase1[3])
+print (phrase1[4])
+print (phrase1[5])
+print (phrase1[6])
+print (phrase1[7])
+
+# Exercice 1 : miles en kilomètres
+# Saisie données utilisateur 
+miles = float(input("Veuillez entrer une distance en miles : "))
+# Conversion (1 mile = 1.60934 kilomètres)
+kilometres = miles * 1.60934
+# Affichage du résultat
+print(f"{miles} miles est équivalent à {kilometres} kilomètres.")
+
+
+# Exercice 2 : variable
+var1 = "Je dois être en seconde position"
+var2 = "Je dois être en première position"
+
+print(var1 + " | " + var2)
+# Utilisation variable temporaire
+temp = var1
+var1 = var2
+var2 = temp
+print(var1 + " | " + var2)
