8ae1a7d6ebcaea85e6f626bdc58e6d0b9cc20c6a
Manu <manu.angot@hotmail.fr>
2023-05-30 13:30:50 +0200

    added first exercises in Python

---
+++ exovariable.py
@@ -0,0 +1,24 @@
+# Variables
+nomDeMaVariable = "la valeur de ma variable"
+nombreUn = 55
+nombreDeux = 5
+
+resultat = nombreUn + nombreDeux
+print(resultat)
+print(nomDeMaVariable)
+
+
+# Booléen
+var1 = True
+var2 = False
+
+print("var1 OU var2 = " + str(var1 or var2))
+print("var1 ET var2 = " + str(var1 and var2))
+print("NON var1 = " + str(not var1))
+
+# concaténation
+mot1 = "Bonjour "
+mot2 = "Python !"
+
+phrase = mot1 + mot2 
+print(phrase)
