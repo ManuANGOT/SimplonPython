b0ea43e6d1b200be49fe0b3eeb913de422586163
Manu <manu.angot@hotmail.fr>
2023-06-07 15:07:02 +0200

    exo_nasa added

--- cours_python/arbres/arbre_class.py
+++ cours_python/arbres/arbre_class.py
@@ -1,42 +1,39 @@
+from __future__ import annotations
+
 class Arbre:
     def __init__(self, value):
         self.value = value
         self.children = []
 
-# Le parcours en profondeur par itération (DFS pour Depth-First Search)
-def iterative_dfs(root):
-    if not root:
-        return
+def _str_(self) -> None :
+    self.value = Value
+    self.children = []
 
-    stack = [root]
+def add_child(self, noeud:Arbre) -> None :
+    self.children.append(noeud)
 
-    while stack:
-        node = stack.pop()
-        print(node.value)
+def is_leaf(self) -> bool :
+    return True if len(self.children) == 0 else False
 
-        # Ajouter les enfants du nœud courant dans la pile
-        for child in reversed(node.children):
-            stack.append(child)
+def obtenir_profondeur(self) -> int :
+    if self.is_leaf():
+        return 1
+    
+    liste_profondeur = [child.obtenir_profondeur() for child in self.children]
+    
+   # Autre façon de faire :
+    # liste_profondeur = []
+    # for child in self.children :
+     #   liste_profondeur.append(child.profondeur())
 
-def create_branch():
-    value = int(input("Valeur du nœud racine : "))
-    root = Arbre(value)
-    stack = [root]
+def dfs(self) -> list :
+    if self.is_leaf():
+        return [self.value]
+    Liste_resultat = []
+    for enfant in self.children:
+        Liste_resultat += enfant.dfs()
+        Liste_resultat.append(self.value)
+        return Liste_resultat
 
-    while stack:
-        node = stack.pop()
-        child_count = int(input(f"Nombre d'enfants pour le nœud {node.value} : "))
 
-        for i in range(child_count):
-            child_value = int(input(f"Valeur de l'enfant {i+1} : "))
-            child = Arbre(child_value)
-            node.children.append(child)
-            stack.append(child)
-
-    return root
-
-# Création de l'arborescence selon la demande de l'utilisateur
-root = create_branch()
-
-# Appel de la fonction de parcours en profondeur itératif
-iterative_dfs(root)
+    
--- cours_python/exo_nasa/exo_nasa.py
+++ cours_python/exo_nasa/exo_nasa.py
@@ -1,1 +1,15 @@
 # Distanciel
+from __future__ import annotations
+import math
+import random
+ 
+class Node:
+    def __init__(self, value:any) -> None:
+        self.value = value
+        self.children = []
+
+    def __str__(self) -> str:
+        return str(self.value)
+    
+    def add_road(self, node:Node) -> None:
+        self.children.append(node)
